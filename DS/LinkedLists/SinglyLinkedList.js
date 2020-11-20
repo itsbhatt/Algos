de40c896729e04fc322c9b3d5827fca72047d020
unknown <sachinbhatt669@gmail.com>
2020-11-21 01:11:00 +0530

    pop added

--- DS/LinkedLists/SinglyLinkedList.js
+++ DS/LinkedLists/SinglyLinkedList.js
@@ -28,33 +28,25 @@ class SinglyLinkedList {
   pop() {
     if (!this.head) return undefined;
 
-    let pre = this.head;
-    let temp = this.head.next;
-
-    if (!temp) {
+    if (!this.head.next) {
       this.head = null;
       this.tail = null;
       this.length = 0;
 
-      return pre;
+      return null;
     }
-    while (temp.next !== null) {
-      //   console.log(temp, 'cfgh');
-      //   const nextEl = temp;
-      pre = temp;
+
+    let temp = this.head;
+    let newTail = temp;
+
+    while (temp.next) {
+      newTail = temp;
       temp = temp.next;
     }
 
-    pre.next = null;
+    newTail.next = null;
 
-    this.tail = pre;
-
-    // console.log({ temp });
-    // console.log({ pre });
-    // if (secondLast) {
-    //   secondLast.next = null;
-    // }
-    // this.tail = secondLast;
+    this.tail = newTail;
 
     this.length -= 1;
 
@@ -64,39 +56,10 @@ class SinglyLinkedList {
 
 const list = new SinglyLinkedList();
 
-// list.push('hi');
-// list.push('there');
-// list.push('!');
-// list.pop();
-// list.pop();
-// list.pop();
-// console.log(list);
-
-// // const a = {
-// //   head: { val: 4, next: { val: 4, next: { val: 4, next: null } } },
-// //   tail: { val: 4, next: null },
-// // };
-
-// const a = { val: 4, next: 5 };
-// const b = a;
-// let c = a;
-
-// const d = { val: 25, next: 12 };
-
-// c.next = d;
-// c = d;
-
-// const e = { val: 23, next: 4232 };
-
-// c.next = e;
-// c = e;
-
-// c.next = null;
-// console.log('====================================');
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// // console.log(JSON.stringify(a));
-// // console.log(JSON.stringify(b));
-// // console.log(JSON.stringify(c));
-// console.log('====================================');
+list.push('hi');
+list.push('there');
+list.push('!');
+list.pop();
+list.pop();
+list.pop();
+console.log(list);
